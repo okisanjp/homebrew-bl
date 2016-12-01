b0464a7ea775d8da37a1fc9ef1550c928104a160
okisanjp <okisan.jp@gmail.com>
2016-12-01 12:34:39 +0900

    Update Formula

--- bl.rb
+++ bl.rb
@@ -2,10 +2,10 @@ require "formula"
 
 class Bl < Formula
   homepage "https://github.com/okisanjp/homebrew-bl"
-  url "https://github.com/okisanjp/homebrew-bl/archive/v1.03.tar.gz"
-  sha256 "41ec177ba14b11281f577aab3ec004a8966a513bf19ebbf53b2b6addd9b6fc3b"
+  url "https://github.com/okisanjp/homebrew-bl/archive/v1.04.tar.gz"
+  sha256 "ea5ec4d884414cee389fa4ce10a4d61c9eb05a408a58f2d1e3df9f57321eba8f"
   head "https://github.com/okisanjp/homebrew-bl.git"
-  version "1.03"
+  version "1.04"
 
   def install
     bin.install "bl"
