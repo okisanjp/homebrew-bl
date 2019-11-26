11d0737b172d07b2f155e3f53cb06aaec0322d1a
jun-okidono <jun-okidono@gmotech.jp>
2019-11-26 16:07:28 +0900

    TarballのURL修正

--- bl.rb
+++ bl.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Bl < Formula
   homepage "https://github.com/okisanjp/homebrew-bl"
-  url "https://github.com/okisanjp/homebrew-bl/archive/v1.05.tar.gz"
+  url "https://github.com/okisanjp/homebrew-bl/archive/1.05.tar.gz"
   sha256 "856e19a81b23749178286f79c56de762754bcb52eb065192834a137b8c4f8d71"
   head "https://github.com/okisanjp/homebrew-bl.git"
   version "1.05"
